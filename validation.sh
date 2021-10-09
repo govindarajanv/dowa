29f33b267a5ca8cbc463e3c90560978efb052416
Govindarajan V <govindarajanv@outlook.com>
2021-10-09 08:00:27 +0000

    added actions validation logic

--- validation.sh
+++ validation.sh
@@ -1,4 +1,14 @@
+arr=("hashicorp/setup-terraform@v1" "myorg/checkout@v2")
+
 for i in $(find ".github/workflows/" -name "*.yml"); 
 do 
-	grep "uses:" $i |awk -F: '{ print $2 }'; 
+	myarr=($(grep "uses:" $i |awk -F: '{ print $2 }'; ))
+done
+for i in "${myarr[@]}"
+do
+   :
+  #echo $i
+  if [[ " ${arr[*]} " == *" $i "* ]]; then
+    echo "$i is an approved action"
+  fi
 done
